d39d51b78fcbe932ae3712722f5eba55d2c3f6e6
Dom <Dom@MAYNTHANGMAYN>
2021-08-14 18:52:30 -0400

    Add project files.

---
+++ WebApplication1/Pages/Forms/AddAddress.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class AddAddressModel : PageModel
+    {
+        [BindProperty]
+        public AddressModel Address { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
